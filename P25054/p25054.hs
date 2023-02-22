a704c25817a41a26bf56c0b30babefe3aab4874a
zhesnaile <carlosojar@gmail.com>
2023-02-22 23:17:34 +0100

    [ADD] primeDivisors

--- P25054/p25054.hs
+++ P25054/p25054.hs
@@ -1,4 +1,3 @@
-
 myLength :: [Int] -> Int
 myLength [] = 0
 myLength(_:xs) = 1 + myLength xs
@@ -6,7 +5,6 @@ myLength(_:xs) = 1 + myLength xs
 myMaximum :: [Int] -> Int
 myMaximum [x] = x
 myMaximum (x:xs) = max x $ myMaximum xs
-
 
 average :: [Int] -> Float
 average ls@(x:xs) =
@@ -35,10 +33,22 @@ oddsNevens xs = (odd_numbers, even_numbers)
         even_numbers = filter even xs
         odd_numbers = filter odd xs
 
-{-
+
 primeDivisors :: Int -> [Int]
-primeDivisors n = [0]
---}
+primeDivisors m = 
+    let
+        isqrt :: Int -> Int
+        isqrt x = floor $ sqrt (fromIntegral x :: Float)
+
+        factors :: Int -> [Int]
+        factors n  = [x | x <- [2..n], mod n x == 0]
+        
+        isPrime :: Int -> Bool
+        isPrime n
+            | n <= 1    = False
+            | otherwise = null [x | x <- [2..isqrt n], mod n x == 0]
+    in 
+        filter isPrime $ factors m
 
 main::IO()
 main = do
@@ -49,6 +59,4 @@ main = do
     print $ remove [1,4,5,3,4,5,1,2,7,4,2] [2,4]
     print $ myLength [1,3..10]
     print $ oddsNevens [1,4,5,3,4,5,1,2,7,4,2]
-{-
     print $ primeDivisors 255
---}

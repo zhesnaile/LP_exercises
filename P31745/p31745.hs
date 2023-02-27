5b06afcb0e752f59323a4875f7ad3a3829ae0004
zhesnaile <carlosojar@gmail.com>
2023-02-27 15:58:49 +0100

    [ADD] firstWord and countIn

--- P31745/p31745.hs
+++ P31745/p31745.hs
@@ -8,11 +8,17 @@ myLength = foldl (\acc _ -> acc +1) 0
 myReverse :: [Int] -> [Int]
 myReverse = foldl (flip (:)) []
 
+countIn :: [[Int]] -> Int -> [Int]
+countIn l x = map (length . filter(==x)) l
+
+firstWord::String -> String
+firstWord =  takeWhile(/=' ') . dropWhile(==' ')
+
 main::IO()
 main = do
     print $ flatten [[1,2,3],[4,5],[6],[],[3,3]]
     print $ myLength "Albert"
     print $ myReverse [1..10]
-{-    print $ countIn [[3,2,3],[3],[], [2,2]] 3
+    print $ countIn [[3,2,3],[3],[], [2,2]] 3
     print $ firstWord "  Volem pa amb oli  "
--}
+{--}
